300ed6d4a001b8c7ac085a1b5d1095766dda495f
limenez <limenez86@gmail.com>
2023-01-11 22:53:59 -0300

    first commit

---
+++ src/ExecutarCasos.java
@@ -0,0 +1,27 @@
+
+public class ExecutarCasos {
+
+	public static void main(String[] args) {
+		//Criando uma variável
+		int dia;
+		//Incrementando a varivel 
+		dia = 1;
+		
+		//Montando uma estrutura de casos
+		switch (dia) {
+		case 1:
+			System.out.println("Olá meu amigo hoje é Domingo");
+			break;
+			
+		case 2:
+			System.out.println("Jesus hoje é segunda-feira");
+			break;
+			
+		case 3:
+			System.out.println("Hoje é terça-feira");
+			break;
+		}
+
+	}
+
+}
